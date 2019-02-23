847f2b35611bd068b5ee71e76234a38491cb2ab7
Albert Jose Kurisummoottil <albert.jose@outlook.com>
2019-02-23 16:01:44 +1030

    sticky nav bar - testing

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,4 +19,17 @@
          loop:true
          
      });
+
+     $(window).scroll(function(){
+        
+       var top = $(window).scrollTop();
+        if(top>=60){
+          $("nav").addClass('secondary'); 
+        }
+        
+        else 
+            if($("nav").hasClass('secondary')){
+                $("nav").removeClass('secondary'); 
+            }
+    });
 });
