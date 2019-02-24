d0f98fd6238b998d24b7f58d0117d545717c11ca
Albert Jose Kurisummoottil <albert.jose@outlook.com>
2019-02-24 11:28:47 +1030

    sticky nav bar - scrolling

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,29 +7,30 @@
        
     });
   
-     $(".section-one-slider-text").typed({
+    $(".section-one-slider-text").typed({
         strings:[
-        "<span><span class='anzte-black'>anz</span><span class='anzte-red btn-light'>te</span><span> -</span><span> Technology People</span></span>",
-        "<span><span class='anzte-black'>anz</span><span class='anzte-red'>te</span><span> -</span><span> A team</span></span>",
-        "<span><span class='anzte-black'>anz</span><span class='anzte-red'>te</span><span> -</span><span> Born out of the BOX</span></span>",
-        "<span><span class='anzte-black'>anz</span><span class='anzte-red'>te</span><span> -</span><span> Make people's lives SIMPLE</span></span>",
-        "<span><span class='anzte-black'>anz</span><span class='anzte-red'>te</span><span> -</span><span> Information Technology and Services Company</span></span>"        
+        // "<span><span class='anzte-black'>anz</span><span class='anzte-red btn-light'>te</span><span> -</span><span> Technology People</span></span>",
+        // "<span><span class='anzte-black'>anz</span><span class='anzte-red'>te</span><span> -</span><span> A team</span></span>",
+        // "<span><span class='anzte-black'>anz</span><span class='anzte-red'>te</span><span> -</span><span> Born out of the BOX</span></span>",        
+        "<span><span class='anzte-black'>anz</span><span class='anzte-red'>te</span><span> -</span><span> Information Technology and Services Company</span></span>",
+        "<span><span class='anzte-black'>anz</span><span class='anzte-red'>te</span><span> -</span><span> Web Development</span></span>",
+        "<span><span class='anzte-black'>anz</span><span class='anzte-red'>te</span><span> -</span><span> Make people's lives SIMPLE</span></span>"
         ],
          typespeed:0,
          loop:true
          
-     });
+    });
 
-     $(window).scroll(function(){
-        
+    $(window).scroll(function(){
        var top = $(window).scrollTop();
         if(top>=60){
           $("nav").addClass('secondary'); 
+          // console.log(top>=60);
         }
-        
         else 
             if($("nav").hasClass('secondary')){
                 $("nav").removeClass('secondary'); 
+                // console.log('test message remove secondary');
             }
     });
 });
