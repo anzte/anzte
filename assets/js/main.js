ca9a7d25dfa4f75cfcb69a4dea409618def4c4d2
Albert Jose Kurisummoottil <albert.jose@outlook.com>
2019-02-26 21:51:24 +1030

    add owl carousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,5 +61,26 @@
     });
 
     //owl carousel
-    $("#team-members").owlCarousel();
+    $("#team-members").owlCarousel({
+      items:3,
+          autoplay:true,
+          smartSpeed:700,
+          // loop:true,
+          autoplayHoverPause:true,
+            responsive:{
+              0:{
+          
+                 items:1
+              }, 
+             480:{
+          
+                 items:2
+              }, 
+                                     
+             768:{
+          
+                 items:3
+              } 
+            }
+      });
 });
