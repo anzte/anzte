51429f25bf758f30d83ae65762a861eeefbdbe07
Albert Jose Kurisummoottil <albert.jose@outlook.com>
2019-03-21 16:23:10 +1030

    anzte - jQuery easing

--- assets/js/main.js
+++ assets/js/main.js
@@ -96,5 +96,16 @@
             }
       });
 
-    $('a.smooth-scroll').smoothScroll();
+    $("a.smooth-scroll").click(function (event) {
+
+        event.preventDefault();
+        
+        var section = $(this).attr("href");
+
+        $('html, body').animate({
+            scrollTop: $(section).offset().top - -2
+        }, 1250, "easeInOutExpo");
+    });
+
+
 });
